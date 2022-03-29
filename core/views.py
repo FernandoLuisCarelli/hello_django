f47c44091d4427cf1d820a11e25273f8592a72cc
FernandoCarelli <49133169+FernandoCarelli@users.noreply.github.com>
2022-03-28 23:03:17 -0300

    Criando rota, que aceite na url, dois paramentros inteiros, retornando a soma dos valores.

--- core/views.py
+++ core/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render, HttpResponse
 
 def hello(request, nome, idade):
     return HttpResponse('<H1>Hello {}  de  {} anos </H1>'.format(nome, idade))
+
+def somar(request, valor1, valor2: object):
+    soma = valor1 + valor2
+    return HttpResponse('A soma dos valores é : {}'.format(soma))
